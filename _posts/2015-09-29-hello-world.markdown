a2f0999dfd9165f7ee541c3135e9801467dc08d5
Abhishek Jain <abhi2000@gmail.com>
2015-11-09 21:54:48 +0000

    Tidy up

---
+++ _posts/2015-09-29-hello-world.markdown
@@ -0,0 +1,14 @@
+---
+layout: post
+title:  "Hello World!!!"
+date:   2015-09-29 13:18:46
+categories: hello world post
+---
+Hello World
+
+{% highlight javascript %}
+    console.log('Hello!!');
+    function(a, b) {
+        return a + b;
+    }
+{% endhighlight %}
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+var runSequence = require('run-sequence');
+var messages = {
+	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+	bs.notify(messages.jekyllBuild);
+	return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+		.on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+	bs.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function () {
+	bs.init({
+		server: {
+			baseDir: '_site'
+		}
+	});
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+	return gulp.src('_sass/main.scss')
+		.pipe(sass({
+			includePaths: ['scss'],
+			onError: bs.notify
+		}))
+		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+		.pipe(gulp.dest('_site/css'))
+		.pipe(bs.stream({once: true}))
+		.pipe(gulp.dest('./css'));
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', function () {
+	runSequence('browser-sync', 'watch');
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+	gulp.watch('_sass/**/*.scss', ['sass']);
+	gulp.watch([
+		'index.html',
+		'_layouts/*',
+		'_includes/*',
+		'about/*',
+		'_posts/*'
+	], ['jekyll-rebuild']);
+});
+
